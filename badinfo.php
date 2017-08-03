916bc1d035b85a0a6221a9e2793aa0a2cfc9e427
poojithakat <pk457@njit.edu>
2017-08-03 04:05:52 -0400

    comit

---
+++ badinfo.php
@@ -0,0 +1,12 @@
+<html>
+
+<body>
+<h1>Wrong Username and Password</h1>
+<h2>Please check your login credentials</h2>
+
+<form method="get" action="https://web.njit.edu/~pk457/final/">
+<button type="submit">Go Back</button>
+</form>
+
+</body>
+</html>
