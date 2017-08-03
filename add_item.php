22476bc6f9d8d214b7f91eabf5b31c539d9f3c2b
poojithakat <pk457@njit.edu>
2017-08-03 03:05:22 -0400

    added add_item.php

---
+++ add_item.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="sample.css">
+</head>
+  <body>
+    <h2> Add to-do item </h2> 
+      <form action='index.php' method='post'>
+        <input type="text" name="todo_item" placeholder="Description">
+	  <input type="date" name="date">
+	    <input type="time" name="time">
+	      <input type="hidden" name="action" value="add_item">
+	        <input type="submit" value="Add" class="button">
+		  </form>
+		    </body>
+		    </html>
