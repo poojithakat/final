b4012527c01f1485fe64fa1676c8608c9fedc88b
poojithakat <pk457@njit.edu>
2017-08-03 02:50:20 -0400

    added login.php

---
+++ login.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+<div class="wrapper">
+<div class="container">
+<h1> To-Do application </h1>
+<h2> Login </h1>
+<form method = "post" action="index.php" class="form">
+<input type="text" name="email" placeholder = "Enter your email">
+<input type="password" name="password" placeholder="Enter your password"> <br> <br>
+<input type ="hidden" name="action" value="test_user">
+<input type="submit" value="Login" id="login-button"/>
+</form>
+<form method = "post" action = "register.php">
+<input type = "submit" value = "Register" />
+</form>
+</div>
+<ul class="bg-bubbles">
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+</ul>
+</div>
+</body>	
+</html>
