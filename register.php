7521c5d31fbaf0fd62ba07c1c08869ffa4f787a9
poojithakat <pk457@njit.edu>
2017-08-03 02:48:58 -0400

    added register.php

---
+++ register.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+<div class="wrapper">
+<div class="container">
+  <h1> Sign Up</h1>
+  <form method = 'post' action = 'index.php'>
+       <input type = "text" name = 'fname'
+       placeholder="firstname"/> <br> </br>
+       <input type= "text" name = 'lname'
+       placeholder="lastname" /> <br> </br>
+       <input type = "text" name='email'
+	placeholder = "email address"/> <br> </br>
+	<input type="password"
+	name='password'value="" placeholder = "password" /> <br> </br>
+	<input type="int"
+	name='ph_number'value="" placeholder = "phone number" /> <br> </br>
+	<input type="date" name='bday'/> <br> </br>
+	<input type="text" name='gender' placeholder="gender"/> <br> </br>
+	<input type="hidden" name= "action" value="register">
+	<br>
+	<input type="submit" value="Register" id="login-button"/>
+  </form>
+  </div>
+  <ul class="bg-bubbles">
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  </ul>
+  </div>
+  </body>
+  </html>
